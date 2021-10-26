356b2c14c0996ba7ff72c7cad2b69f1e7b3ee1c1
Natanael <natanaelbandeira@gmail.com>
2021-10-25 19:25:52 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+var parseurl = require('parseurl')
+var session = require('express-session')
+
+const app = express()
+const port = 5000
+
+// Static Files
+app.use(express.static('public'))
+
+// Templating Engine
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+// Routes
+const comicsRouter = require('./src/routes/comic')
+
+// Storing view data temporarily
+app.use(session({
+    secret: 'stratus 360',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(function (req, res, next) {
+    if (!req.session.views) {
+        req.session.views = {}
+    }
+
+    // get the url pathname
+    var pathname = parseurl(req).pathname
+
+    // count the views
+    req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+
+    next()
+})
+
+app.use('/', comicsRouter)
+
+// Listen on port 5000
+app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ src/routes/comic.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const comicsRouter = express.Router()
+const axios = require('axios')
+
+
+comicsRouter.get('', async (req, res) => {
+    try {
+        const xkcdCurrentAPI = await axios.get(`https://xkcd.com/info.0.json`)
+        const xkcdCurrentData = xkcdCurrentAPI.data
+        res.render('comic', {
+            data: xkcdCurrentData,
+            num: xkcdCurrentData.num,
+            currentNum: xkcdCurrentData.num,
+            random: randomComicNumber(1, xkcdCurrentData.num),
+            userCount: req.session.views['/']
+        })
+    }
+    catch (err) {
+        if (err.response) {
+            res.render('comic', { data: null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+        } else if (err.request) {
+            res.render('comic', { data: null })
+            console.log(err.request)
+        } else {
+            res.render('comic', { data: null })
+            console.error('Error', err.message)
+        }
+    }
+})
+
+comicsRouter.get('/:num', async (req, res) => {
+    let comicNum = req.params.num
+    try {
+        const xkcdCurrentAPI = await axios.get(`https://xkcd.com/info.0.json`)
+        const xkcdCurrentData = xkcdCurrentAPI.data
+
+        const xkcdAPI = await axios.get(`https://xkcd.com/${comicNum}/info.0.json`)
+        const xkcdData = xkcdAPI.data
+
+
+        if (xkcdCurrentData.num !== xkcdData.num) {
+            res.render('comic', {
+                data: xkcdData,
+                num: xkcdData.num,
+                currentNum: xkcdCurrentData.num,
+                random: randomComicNumber(1, xkcdCurrentData.num),
+                userCount: req.session.views[`/${xkcdData.num}`]
+            })
+        } else {
+            res.redirect('/')
+        }
+    }
+    catch (err) {
+        if (err.response) {
+            res.render('comic', { data: null })
+            console.log(err.response.data)
+            console.log(err.response.status)
+        } else if (err.request) {
+            res.render('comic', { data: null })
+            console.log(err.request)
+        } else {
+            res.render('comic', { data: null })
+            console.error('Error', err.message)
+        }
+    }
+})
+
+function randomComicNumber(first, current) {
+    return Math.floor(Math.random() * (current - first + 1) + first);
+}
+
+function readableTranscript(transcript) {
+    let dataToArray = transcript.split(']]').map
+}
+
+module.exports = comicsRouter
